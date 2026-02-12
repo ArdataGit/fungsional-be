0b9e8fa444487b3fd36f59ed6cea8e3e7bc0546f
“ardatapkl” <ardatapkl@gmail.com>
2026-02-12 13:01:11 +0700

    edit profile

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -9,12 +9,14 @@ const changeProfile = async (req, res, next) => {
   try {
     const schema = Joi.object({
       name: Joi.string().required(),
-      email: Joi.string().email().required(),
+      email: Joi.string().email(),
       noWA: Joi.string().required(),
       alamat: Joi.string().required(),
       provinsi: Joi.string().required(),
       kabupaten: Joi.string().required(),
       kecamatan: Joi.string().required(),
+      jurusan: Joi.string().allow('', null),
+      ttl: Joi.string().allow('', null),
       gambar: Joi.string(),
     }).unknown(true);
 
@@ -41,12 +43,14 @@ const changeProfile = async (req, res, next) => {
       },
       data: {
         name: validate.name,
-        email: validate.email, // perbaikan ✅
+        // email: validate.email, // perbaikan ✅
         noWA: validate.noWA,
         alamat: validate.alamat,
         provinsi: validate.provinsi,
         kabupaten: validate.kabupaten,
         kecamatan: validate.kecamatan,
+        jurusan: validate.jurusan,
+        ttl: validate.ttl,
         gambar: validate.gambar || isExist.gambar,
       },
     });
